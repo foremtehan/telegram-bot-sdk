574801c1ea17ef79e9635f11caf2895a6dc58c21
foremtehan <foremtehan@gmail.com>
2025-04-25 13:35:15 +0330

    first

---
+++ tests/Traits/CommandGenerator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Telegram\Bot\Tests\Traits;
+
+use Illuminate\Support\Collection;
+use Telegram\Bot\Commands\Command;
+
+trait CommandGenerator
+{
+    /**
+     * @param  int  $numberRequired
+     */
+    private function commandGenerator($numberRequired): Collection
+    {
+        $range = range(1, $numberRequired, 1);
+
+        return collect($range)
+            ->map(fn (int $instanceNumber): Command => new class($instanceNumber) extends Command
+            {
+                private int $instanceNumber;
+
+                public function __construct(int $instanceNumber)
+                {
+                    $this->instanceNumber = $instanceNumber;
+                }
+
+                public function getName(): string
+                {
+                    return sprintf('MockCommand%s', $this->instanceNumber);
+                }
+
+                public function getAliases(): array
+                {
+                    return [sprintf('MockAlias%s', $this->instanceNumber)];
+                }
+
+                public function handle(): void {}
+            });
+    }
+}
---
+++ tests/Traits/GuzzleMock.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Telegram\Bot\Tests\Traits;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Collection;
+use Telegram\Bot\HttpClients\GuzzleHttpClient;
+
+trait GuzzleMock
+{
+    /**
+     * This collection contains a history of all requests and responses
+     * sent using the client.
+     */
+    protected array $history = [];
+
+    public function getGuzzleHttpClient(array $responsesToQueue = []): GuzzleHttpClient
+    {
+        $client = $this->createClientWithQueuedResponse($responsesToQueue);
+
+        return new GuzzleHttpClient($client);
+    }
+
+    protected function createClientWithQueuedResponse(array $responsesToQueue): Client
+    {
+        $this->history = [];
+        $handler = HandlerStack::create(new MockHandler($responsesToQueue));
+        $handler->push(Middleware::history($this->history));
+
+        return new Client(['handler' => $handler]);
+    }
+
+    public function makeFakeServerResponse(bool|array $data, int $status_code = 200, array $headers = []): Response
+    {
+        return new Response(
+            $status_code,
+            $headers,
+            json_encode([
+                'ok' => true,
+                'result' => $data,
+            ])
+        );
+    }
+
+    public function makeFakeInboundUpdate(array $data, $status_code = 200, $headers = []): Response
+    {
+        return new Response(
+            $status_code,
+            $headers,
+            json_encode($data)
+        );
+    }
+
+    public function getHistory(): Collection
+    {
+        return collect($this->history);
+    }
+
+    protected function makeFakeServerErrorResponse($error_code, $description, $status_code = 200, $headers = []): Response
+    {
+        return new Response(
+            $status_code,
+            $headers,
+            json_encode([
+                'ok' => false,
+                'error_code' => $error_code,
+                'description' => sprintf('%s', $description),
+            ])
+        );
+    }
+
+    //    protected function makeFakeExceptionResponse($text, $uri)
+    //    {
+    //        return new RequestException($text, new Request('GET', $uri));
+    //    }
+}
